42de9418153bb51d277ead3618fa7357a08cee6c
susanasilvas <159494432+susanasilvas@users.noreply.github.com>
2024-03-07 16:53:29 -0600

    Add files via upload

---
+++ EVA2_3_IF.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_3_if;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class EVA2_3_IF {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      double califa;
+      Scanner captu = new Scanner(System.in);
+      califa = captu.nextDouble();
+      if (califa >= 70){
+          System.out.println("Aprobaste!!!");
+        }  else//la evaluacion fase es opcional
+      System.out.println("No Aprobaste!!!");
+      
+          
+      
+    }
+    
+}
