f0c15f6a2f88cb81d5a7b4b6cd74b17249a2b228
susanasilvas <159494432+susanasilvas@users.noreply.github.com>
2024-04-09 16:51:38 -0600

    Add files via upload

---
+++ EVA2_18_FOR.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_18_for;
+
+/**
+ *
+ * @author user
+ */
+public class EVA2_18_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // // INICIO; CONDICION; INCREMENTO/ DECREMENTO
+        for(int i = 1; i <= 10; i++ ){
+            // LLAVES SON OPCIONALES 
+            //TENGAN MÁS DE UNA ISTRUCCIÓN A REPETIR
+            System.out.print(i + "-");
+            }
+        
+          System.out.println("");
+            for(int i = 10; i >= 1; i-- ){
+            // LLAVES SON OPCIONALES 
+            //TENGAN MÁS DE UNA ISTRUCCIÓN A REPETIR
+            System.out.print(i + "-");
+            // IMPRIMIR LOS NUMEROS PARES ENTRE 0 Y 100
+                        }
+
+           
+            // IMPRIMIR LOS NUMEROS PARES ENTRE 0 Y 100
+            // 0, 2 4 6 8 10
+            System.out.println("");
+            for(int i = 0; i <= 100; i+=2 ){
+                System.out.print(i + "-");
+          
+            
+                
+            }            
+
+            
+        }
+            
+    }
+    
+
