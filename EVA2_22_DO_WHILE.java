768c2a9b24bf6b9a4d24e4614f38e743762ac6fa
susanasilvas <159494432+susanasilvas@users.noreply.github.com>
2024-04-15 16:49:40 -0600

    Add files via upload

---
+++ EVA2_22_DO_WHILE.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_22_do_while;
+
+/**
+ *
+ * @author user
+ */
+public class EVA2_22_DO_WHILE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int cont = 11;
+        do{
+        System.out.print(cont + "-");
+        cont++;
+        }while(cont <= 10 );
+        
+        
+    }
+    
+}
