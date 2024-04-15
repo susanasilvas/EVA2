10e571e9e519d61cea752c210fee5989807e9464
susanasilvas <159494432+susanasilvas@users.noreply.github.com>
2024-04-15 16:50:09 -0600

    Add files via upload

---
+++ EVA2_24_ADIVINAR_WHILE.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_24_adivinar_while;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class EVA2_24_ADIVINAR_WHILE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int nume = 5; //valor para almacenar lo que capture el usuario
+        int valor = 0;
+        Scanner captu = new Scanner(System.in);
+        while(nume != valor);{
+            System.out.println("Cual es el numero(entre 1 y 10)");
+       valor = captu.nextInt();
+       }
+       System.out.println("ADIVINATE!!")
+       
+    }
+    
+}
